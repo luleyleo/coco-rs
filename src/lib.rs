4f0947c2f2d6570c252d4660d91067c9ca861707
Leopold Luley <git@leopoldluley.de>
2021-12-23 10:13:53 +0900

    Add random state.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use coco_sys::{coco_observer_t, coco_problem_t, coco_suite_t};
+use coco_sys::{coco_observer_t, coco_problem_t, coco_random_state_t, coco_suite_t};
 use std::{
     ffi::{CStr, CString},
     ops::RangeInclusive,
@@ -255,6 +255,38 @@ impl Drop for Observer {
     fn drop(&mut self) {
         unsafe {
             coco_sys::coco_observer_free(self.inner);
+        }
+    }
+}
+
+/// COCO specific random number generator.
+pub struct RandomState {
+    inner: *mut coco_random_state_t,
+}
+
+impl RandomState {
+    /// Creates a new random number state using the given seed.
+    pub fn new(seed: u32) -> Self {
+        let inner = unsafe { coco_sys::coco_random_new(seed) };
+
+        RandomState { inner }
+    }
+
+    /// Generates an approximately normal random number.
+    pub fn normal(&mut self) -> f64 {
+        unsafe { coco_sys::coco_random_normal(self.inner) }
+    }
+
+    /// Returns one uniform [0, 1) random value.
+    pub fn uniform(&mut self) -> f64 {
+        unsafe { coco_sys::coco_random_uniform(self.inner) }
+    }
+}
+
+impl Drop for RandomState {
+    fn drop(&mut self) {
+        unsafe {
+            coco_sys::coco_random_free(self.inner);
         }
     }
 }
